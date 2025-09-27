64cb1c61713c6642db98cecf9c8d3248e21ea932
doginvi <victordesousaestudo@gmail.com>
2025-09-27 20:01:14 -0300

    FIX: Correção do exercício 6, afinal não havia sido aprendido módulo ainda

--- primeiro-mundo/ex006.py
+++ primeiro-mundo/ex006.py
@@ -1,11 +1,9 @@
 # Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 
-import math;
-
 numero = float(input('Digite um valor qualquer: '));
 
 dobro = numero * 2;
 triplo = numero * 3;
-raizQuadrada = math.sqrt(numero);
+raizQuadrada = numero ** (1/2);
 
-print(f'O número {numero:.2f}:\nO dobro deste número é: {dobro:.2f}\nO triplo deste número é: {triplo:.2f}\nA raiz quadrada deste número é {raizQuadrada:.2f}');
+print(f'O número {numero:.2f}:\nO dobro deste número é: {dobro:.2f}\nO triplo deste número é: {triplo:.2f}\nA raiz quadrada deste número é {raizQuadrada:.1f}');
