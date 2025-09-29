12da97ca5b04abb19ca503bbfebb457aa74050db
doginvi <victordesousaestudo@gmail.com>
2025-09-28 22:33:59 -0300

    FEAT: Exercício 20 resolvido, método shuffle foi utilizado

--- primeiro-mundo/ex020.py
+++ primeiro-mundo/ex020.py
@@ -1,1 +1,14 @@
 # Exercício Python 20: O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+import random
+
+primeiroAluno = str(input("Digite o nome do aluno: "));
+segundoAluno = str(input("Digite o nome do aluno: "));
+terceiroAluno = str(input("Digite o nome do aluno: "));
+quartoAluno = str(input("Digite o nome do aluno: "));
+
+alunos = [primeiroAluno, segundoAluno, terceiroAluno, quartoAluno];
+
+random.shuffle(alunos);
+
+print(alunos);
