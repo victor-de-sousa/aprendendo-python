3273688f52c7f4072be893d05e5d9c08674860c4
doginvi <victordesousaestudo@gmail.com>
2025-09-29 20:47:21 -0300

    FEAT: Arquivo que reproduz uma música

--- primeiro-mundo/ex021.py
+++ primeiro-mundo/ex021.py
@@ -1,1 +1,5 @@
 # Exercício Python 21: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+
+from python_play.player import play_it
+
+play_it('C:/Users/ADMIN/Music/Interessante.mp3')
