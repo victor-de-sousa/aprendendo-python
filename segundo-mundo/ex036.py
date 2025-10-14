9be6213ca8f9d3b4bbc4e0e25bb784f5ec7cac81
doginvi <victordesousaestudo@gmail.com>
2025-10-14 11:39:12 -0300

    WIP: Exercícios 36 e 37 finalizados, falta o restante

--- segundo-mundo/ex036.py
+++ segundo-mundo/ex036.py
@@ -1,1 +1,14 @@
 # Exercício Python 36: Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
+
+valorCasa = float(input("Qual o valor da casa? "));
+salario = float(input("Qual o seu salário? "));
+anos = int(input("Em quantos anos você deseja pagar a casa? "));
+
+prestacaoMensal = (valorCasa / (anos * 12));
+
+if prestacaoMensal > (salario * 0.3):
+    print("O empréstimo foi negado");
+
+else:
+    print("O empréstimo foi aceito");
+
--- segundo-mundo/ex037.py
+++ segundo-mundo/ex037.py
@@ -1,1 +1,19 @@
 # Exercício Python 37: Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
+
+numeroInteiro = int(input("Digite o número inteiro: "));
+baseConversao = int(input("""Insira a base de conversão:
+    1 - Binário
+    2 - Octal
+    3 - Hexadecimal
+    Digite um dos valores citados: """));
+
+if baseConversao > 0 and baseConversao < 4:
+    if baseConversao == 1:
+        print(f"O número {numeroInteiro} em binário é: {numeroInteiro:b}");
+    elif baseConversao == 2:
+        print(f"O número {numeroInteiro} em Octal é: {numeroInteiro:o}");
+    else:
+        print(f"O número {numeroInteiro} em hexadecimal é: {numeroInteiro:x}");
+
+else:
+    print("Valor inserido para a base de conversão incorreto");
