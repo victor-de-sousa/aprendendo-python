c63d69353cf84d7f4c3312da96f8d280182d5d1d
doginvi <victordesousaestudo@gmail.com>
2025-10-15 13:21:44 -0300

    FEAT: Aula repetição de valores

---
+++ segundo-mundo/estruturaRepeticaoFor.py
@@ -0,0 +1,9 @@
+# Estrutura de Repetição For
+
+# for [qualquer-valor] in range([valorInicial], [valorFinal], [opcional-pulaNumeros]);
+
+# adicao = 0
+# for repeticao in range(1, 10, 2):
+#     adicao += 1;
+
+# print(adicao);
