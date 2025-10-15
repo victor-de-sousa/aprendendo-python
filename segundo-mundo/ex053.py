aded1182d2b08173c813a975913373d1a01dbf7c
doginvi <victordesousaestudo@gmail.com>
2025-10-15 13:33:48 -0300

    FEAT: Criação dos exercícios

---
+++ segundo-mundo/ex053.py
@@ -0,0 +1,4 @@
+# Exercício Python 53: Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços. Exemplos de palíndromos:
+
+# APÓS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, O LOBO AMA O BOLO, ANOTARAM A DATA DA MARATONA.
+
---
+++ segundo-mundo/ex054.py
@@ -0,0 +1,1 @@
+# Exercício Python 54: Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
