218ec2142d24026796bd017ce7d4c6c73bd6db1e
doginvi <victordesousaestudo@gmail.com>
2025-10-16 19:00:42 -0300

    FIX: ex055.py, refiz sem usar listas

--- segundo-mundo/ex055.py
+++ segundo-mundo/ex055.py
@@ -1,12 +1,16 @@
 # Faça um programa que leia o peso de 5 pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 
-pesos = [];
+maiorPeso = 0;
+menorPeso = 0;
 
 for quantidade in range(0, 5):
     peso = int(input("Digite o seu peso: "));
-    pesos.append(peso);
+    if quantidade == 0:
+        menorPeso = peso;
+    if maiorPeso < peso:
+        maiorPeso = peso;
+    elif menorPeso > peso:
+        menorPeso = peso;
 
-pesos.sort();
-
-print(f"""O maior peso lido foi: {pesos[-1]}
-O menor peso lido foi {pesos[0]}"""); 
+print(f"""O maior peso inserido foi de: {maiorPeso}
+E o menor peso inserido foi de: {menorPeso}""");
