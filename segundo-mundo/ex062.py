e0a673299585c36633b6b751845e9e1aad592884
doginvi <victordesousaestudo@gmail.com>
2025-10-19 17:04:16 -0300

    WIP: Resolução de alguns exercícios

--- segundo-mundo/ex062.py
+++ segundo-mundo/ex062.py
@@ -1,1 +1,20 @@
 # Melhore o DESAFIO 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerra quando ele disser que quer mostrar 0 termos.
+
+primeiroTermo = int(input("Digite o primeiro termo da PA: "));
+razao = int(input("Digite a razão da PA: "));
+
+termo = 0;
+quantidadePosicoes = 0;
+termoUser = 10
+
+while termoUser > 0:
+
+    while quantidadePosicoes < termoUser:
+        quantidadePosicoes += 1;
+        termo = primeiroTermo + (quantidadePosicoes - 1) * razao;
+        print(termo);
+    
+    termoUser = int(input("Você deseja mostrar mais quantos termos? "));
+
+    if termoUser != 0:
+        termoUser += quantidadePosicoes; 
--- segundo-mundo/ex063.py
+++ segundo-mundo/ex063.py
@@ -3,3 +3,25 @@
 # Ex:
 
 # 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8
+
+
+
+numero = int(input("Digite um número qualquer: "));
+
+primeiroNumero = 0;
+segundoNumero = 1;
+
+fibonacci = 1;
+
+contador = 0;
+
+print(fibonacci);
+
+while contador != numero:
+    contador+=1;
+    fibonacci = primeiroNumero + segundoNumero;
+    print(fibonacci);
+    primeiroNumero = segundoNumero;
+    segundoNumero = fibonacci;
+
+print("--FIM DA SEQUÊNCIA--");
