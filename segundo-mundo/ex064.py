9cc0223f6888f18a9538320591215fb97de08a9c
doginvi <victordesousaestudo@gmail.com>
2025-10-20 19:25:14 -0300

    WIP: Exercícios resolvidos

--- segundo-mundo/ex064.py
+++ segundo-mundo/ex064.py
@@ -1,2 +1,15 @@
 # Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag)
 
+numero = 0;
+contador = 0;
+soma = 0;
+
+while numero != 999:
+    numero = int(input("Digite vários números inteiros: "));
+    if numero != 999:
+        contador += 1;
+        soma += numero;
+        
+
+
+print(f"Foram digitados cerca de {contador} números e a soma entre todos eles é igual à: {soma}");
--- segundo-mundo/ex065.py
+++ segundo-mundo/ex065.py
@@ -1,1 +1,36 @@
 # Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+
+contador = 0;
+soma = 0;
+media = 0;
+maior = 0;
+menor = 0;
+
+while contador != 10:
+    contador+=1;
+    numero = int(input("Insira um número para continuar: "));
+    soma += numero;
+    media += 1;
+
+    if contador == 1:
+        menor = numero;
+
+    if numero > maior:
+        maior = numero;
+
+    elif numero < menor:
+        menor = numero;
+
+    if contador == 10:
+        media = soma / media;
+        print(f"O maior número é: {maior}");
+        print(f"O menor número é: {menor}");
+        print(f"A média entre todos os números é igual à {media:.2f}");
+        
+        continuarUsuario = '';
+        
+        while continuarUsuario != 'S' and continuarUsuario != 'N':
+            continuarUsuario = str(input("Você deseja continuar a digitar valores? [S/N] "));
+            
+        if continuarUsuario == "S":
+            contador = 2;
